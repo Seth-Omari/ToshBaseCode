5b6d41329fb2945cc512b18a0c6db0b88676408c
Anthony Ngure <anthonyngure25@gmail.com>
2017-09-19 12:06:33 +0300

    Added activity exit and enter animations

---
+++ toshbasecode/src/main/java/ke/co/toshngure/basecode/app/TestSnapShot.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * Anthony Ngure
+ *
+ * Email : anthonyngure25@gmail.com
+ */
+
+package ke.co.toshngure.basecode.app;
+
+/**
+ * Created by Anthony Ngure on 19/09/2017.
+ * Email : anthonyngure25@gmail.com.
+ */
+
+public class TestSnapShot {
+
+    public static void testing() {
+
+    }
+}
